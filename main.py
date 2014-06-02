d6b7947173f1db23d0fe58e3c3a4e9f8bc64e34a
Daniel Cole <dancole42@gmail.com>
2014-06-02 00:05:58 +0000

    Support for multiple user auths

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        auth = ['dancole42@gmail.com',
+                'jordana20@gmail.com']
         user = users.get_current_user()
         #if user:
         #    greeting = ('Welcome, %s - %s! (<a href="%s">sign out</a>)' %
         #                (user.nickname(), user.email(), users.create_logout_url('/')))
-        if user.email() == 'dancole42@gmail.com':   
-            greeting = ('Welcome, %s - %s! (<a href="%s">sign out</a>)' %
+        if user:
+          if user.email() in auth:   
+          greeting = ('Welcome, %s - %s! (<a href="%s">sign out</a>)' %
                         (user.nickname(), user.email(), users.create_logout_url('/')))
+          else:
+            greeting = ('<a href="%s">You are not authorized to access this reportt</a>.' %
+                        users.create_login_url('/'))
         else:
-            greeting = ('<a href="%s">Sign in or register</a>.' %
+            greeting = ('<a href="%s">You must sign in to access this report</a>.' %
                         users.create_login_url('/'))
 
         self.response.out.write('<html><body>%s</body></html>' % greeting)
